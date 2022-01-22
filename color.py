2a5e9a66ca68857541eea17459b992c7a618ee19
Halal-Dev <RippedMario@gmail.com>
2022-01-22 11:57:32 +0100

    SUS

--- color.py
+++ color.py
@@ -1,4 +1,12 @@
 from Vectors import Vector3D
 
 class Color(Vector3D):
-    pass
+    """Stores Colors as RGB triplets - same as vectors"""
+    
+    @classmethod
+    def from_hex(cls,hexcolor="#000000"):
+        x = int(hexcolor[1:3], 16) /255.0
+        y = int(hexcolor[3:5], 16) /255.0
+        z = int(hexcolor[5:7], 16) /255.0
+        return cls(x,y,z)
+        
--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 """Puray - Pure Python Ray Tracing algorithm written by Y"""
 from image import Image
 from color import Color
-
+from Vectors import Vector3D
 def main():
-    WIDTH = 3
-    HEIGHT = 2
-    im = Image(WIDTH, HEIGHT)
-    red =Color(x=1, y=0, z=0)
+    WIDTH = 320
+    HEIGHT = 200
+    camera = Vector3D(0,0,-1)
+    objects = [Sphere(Point(0,0,0), 0.5), Color.from_hex("#FF0000")]
+    scene = Scene(camera, objects, WIDTH, HEIGHT)
+    engine = RenderEngine()
+    image = engine.render(scene)
+    with open("test.ppm", 'w') as img_file:
+        image.write_ppm(img_file)
 
 if __name__ == "__main__":
     main()
